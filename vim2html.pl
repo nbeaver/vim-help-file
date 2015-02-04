ec7843e1300054e459a6878a9274c392d2cd98ab
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-02-04 05:17:29 -0600

    More editing.

--- vim2html.pl
+++ vim2html.pl
@@ -87,6 +87,7 @@ sub vim2html
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <title>VIM: $outfile</title>
 <link rel="stylesheet" href="vim-stylesheet.css" type="text/css">
 </head>
